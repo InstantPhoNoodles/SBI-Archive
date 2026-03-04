6d7e789d62ec7e42873c95dba1da5c5db2184748
Edgar Barron <genericemail.org@gmail.com>
2026-03-04 01:47:50 -0800

    added emmbed videos to SBI2

--- src/SBI2.tsx
+++ src/SBI2.tsx
@@ -1,4 +1,5 @@
 import PlayerCard from "./PlayerCard"
+import YouTubePlayer from "./YoutubePlayer";
 import "./SBI1.css"
 import Lance from "/lance_cropped.jpg";
 import Dylan from "/dylan.jpg";
@@ -37,6 +38,19 @@ export default function SBI2() {
                     <PlayerCard name="Kenma" image={Ken} />
                     <PlayerCard name="Futaboy" image={Mirza} />
                 </div>
+
+                <div><h1>SBI 2 Pre-Game Interview</h1></div>
+                <YouTubePlayer videoId="5UluUYEOeBU?si=Y2L_1Oi8_3xyoSQs"></YouTubePlayer>
+
+                <div><h1>SBI 2 Game 1 + Draft</h1></div>
+                <YouTubePlayer videoId="fBq-YBGmC2M?si=WRREe495Zwk9opML"></YouTubePlayer>
+
+                <div><h1>SBI 2 Game 2 + Draft + Post-Game</h1></div>
+                <YouTubePlayer videoId="G6IAH0UkLYo?si=OIC9lCmyM4FfFZ6Z"></YouTubePlayer>
+
+                <div><h1>SBI 2 Draft 3</h1></div>
+                <YouTubePlayer videoId="BH4xQw27y90?si=Olk3miwm99yxFlVS"></YouTubePlayer>
+                
             </div>
         </div>
     )
--- src/YouTubePlayer.tsx
+++ src/YouTubePlayer.tsx
@@ -1,15 +1,20 @@
-export default function YouTubePlayer() {
+interface YouTubePlayerProps {
+  videoId: string;
+}
+
+
+export default function YouTubePlayer({ videoId }: YouTubePlayerProps) {
   return (
     <div className="p-500">
       <iframe
         width="100%"
-        height="1300"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        title="YouTube video"
+        height="1080"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowFullScreen>
+      </iframe>
     </div>
   );
 }
