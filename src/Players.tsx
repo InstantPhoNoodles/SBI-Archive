00d0f08c3d3be9c2e641a65aca11c3ddb22ebd13
Edgar Barron <genericemail.org@gmail.com>
2026-03-22 15:20:51 -0700

    updated players page and fonts and styling

--- src/Players.tsx
+++ src/Players.tsx
@@ -21,28 +21,36 @@ export default function Players() {
 
         <div className="PlayersPage">
             <div className="PlayersPageBanner">
-                <h1>Meet Your Players</h1>
+                <h1>Meet the Players</h1>
             </div>
 
             <div className="PlayerCardContainer">
-                <PlayerCard name="Choke" image={Jevoni} rings="☆ ☆ ☆" borderColor="goldenrod" info="Mickey Mouse Hall of Famer"/>
-                <PlayerCard name="Coby" image={Coby} rings="☆ ☆ ☆" borderColor="goldenrod" info="goat"/>
-                <PlayerCard name="Yaastoevsky" image={Dylan} rings="☆ ☆ ☆" borderColor="goldenrod" info="beautiful"/>
-                <PlayerCard name="Kino" image={Kino} rings="☆ ☆ ☆" borderColor="goldenrod" info="cho gath"/>
+                <div className="title"><h2>☆ ☆ ☆ Hall of Fame ☆ ☆ ☆</h2></div>
 
-                <PlayerCard name="Toyote" image={Gabe} rings="☆☆" borderColor="silver" info="gorgeous"/>
-                <PlayerCard name="Munson" image={Munson} rings="☆☆" borderColor="silver" info="bard"/>
+                <div className="hall_of_fame">
+                    <PlayerCard name="Choke" image={Jevoni} rings="☆ ☆ ☆" borderColor="goldenrod" info="Mickey Mouse Hall of Famer"/>
+                    <PlayerCard name="Coby" image={Coby} rings="☆ ☆ ☆" borderColor="goldenrod" info="goat"/>
+                    <PlayerCard name="Yaastoevsky" image={Dylan} rings="☆ ☆ ☆" borderColor="goldenrod" info="beautiful"/>
+                    <PlayerCard name="Kino" image={Kino} rings="☆ ☆ ☆" borderColor="goldenrod" info="cho gath"/>
+                </div>
 
-                <PlayerCard name="Swegy" image={Lance} rings="☆" borderColor="rgb(204, 92, 0)" info="white boy with motion"/>
-                <PlayerCard name="Kenma" image={Ken} rings="☆" borderColor="rgb(204, 92, 0)" info="kenmanutfitinhermouth?"/>
-                <PlayerCard name="Futaboy" image={Mirza} rings="☆" borderColor="rgb(204, 92, 0)" info="pedo"/>
-                <PlayerCard name="Marcy" image={Marcy} rings="☆" borderColor="rgb(204, 92, 0)" info="lucifer"/>
-                <PlayerCard name="Colada" image={Rohan} rings="☆" borderColor="rgb(204, 92, 0)" info="indian"/>
-                <PlayerCard name="Sheen" image={Sean} rings="☆" borderColor="rgb(204, 92, 0)" info="beloved"/>
-                <PlayerCard name="Squidlette" image={Naomi} rings="☆" borderColor="rgb(204, 92, 0)" info="naomi??"/>
+                <div className="divider"></div>
+
+                <div className="players">
+                    <PlayerCard name="Toyote" image={Gabe} rings="☆☆" borderColor="silver" info="gorgeous"/>
+                    <PlayerCard name="Munson" image={Munson} rings="☆☆" borderColor="silver" info="bard"/>
                 
-                <PlayerCard name="Suckonemvulvavic " image={Victor} rings="-" borderColor="black" info="deceased"/>
-                <PlayerCard name="ZeroAcesLeft" image={Sarah} rings="-" borderColor="black" info="????????"/>
+                    <PlayerCard name="Swegy" image={Lance} rings="☆" borderColor="rgb(204, 92, 0)" info="white boy with motion"/>
+                    <PlayerCard name="Kenma" image={Ken} rings="☆" borderColor="rgb(204, 92, 0)" info="kenmanutfitinhermouth?"/>
+                    <PlayerCard name="Futaboy" image={Mirza} rings="☆" borderColor="rgb(204, 92, 0)" info="pedo"/>
+                    <PlayerCard name="Marcy" image={Marcy} rings="☆" borderColor="rgb(204, 92, 0)" info="lucifer"/>
+                    <PlayerCard name="Colada" image={Rohan} rings="☆" borderColor="rgb(204, 92, 0)" info="indian"/>
+                    <PlayerCard name="Sheen" image={Sean} rings="☆" borderColor="rgb(204, 92, 0)" info="beloved"/>
+                    <PlayerCard name="Squidlette" image={Naomi} rings="☆" borderColor="rgb(204, 92, 0)" info="naomi??"/>
+
+                    <PlayerCard name="Suckonemvulvavic " image={Victor} rings="-" borderColor="black" info="deceased"/>
+                    <PlayerCard name="ZeroAcesLeft" image={Sarah} rings="-" borderColor="black" info="????????"/>
+                </div>
             </div>
         </div>
     )
